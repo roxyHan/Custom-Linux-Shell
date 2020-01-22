45bcdd49a7fcbc742559791e834844fff58c230c
roxyHan <djelia96@gmail.com>
2020-01-22 10:06:12 -0800

    Version 2--- Fully functionning version! includes: => Responsive to built-in functions such as    ls -lha => Responsive to this shell's internal functions : quit, change_prompt, distro and info. All of these commands have variants that will be specified later with the help command.

--- cwushell.cpp
+++ cwushell.cpp
@@ -7,8 +7,8 @@
 #include <vector>
 #include <iostream>
 #include <cstring>
-#include <vector>
 #include <unistd.h>
+#include <wait.h>
 #include "cwushell.h"
 
 using namespace std;
@@ -30,7 +30,7 @@ void cwushell::mechanism() {
 
     do {
         // 1. Print a prompt
-        printf("cwushell > ");
+        cout << default_prompt;
 
         // 2. Read a line of input from the user
         getline(cin, line);
@@ -45,29 +45,49 @@ void cwushell::mechanism() {
 
         //--- iterate through the vector
         bool progName_end = true;
+        myParameters[allParams.size()];
+        int i = 0;
         vector<string>::iterator it = allParams.begin();
         for (it; it != allParams.end(); ++it) {
             if (progName_end) {
                 prog_name = *it;
                 progName_end = false;
+                myParameters[0] = const_cast<char *>((*it).c_str());
+                i++;
             }
             else{
                 params.push_back(*it);
+                myParameters[i] = const_cast<char*>((*it).c_str());
+                i++;
             }
         }
-        allParams.clear();
-        free(args);
 
-        cout << prog_name << endl;
+        /**cout << prog_name << endl;
         cout << "Now the command to be executed" << endl;
         for (vector<string>::iterator it = params.begin(); it != params.end(); ++it) {
             cout << *it << " ";
-        }
+        }*/
+
 
         // 4. Use the fork() system call to spawn a new child process
-            codeFlag = execute(params);
+            //---- Check if the user input matches one predefined command
+
+            if (prog_name.find(exit1) != string::npos) {
+                quit(prog_name, allParams.size());
+            } else if (prog_name.find(change_prompt) != string::npos) {
+                change(prog_name, allParams.size());
+            } else if (prog_name.find("distro") != string::npos) {
+                distro(prog_name, myParameters[1]);
+            } else if (prog_name.find("info") != string::npos) {
+                info(prog_name, myParameters[1]);
+            } else {
+                codeFlag = execute(myParameters);
+            }
+
 
         // 5. Repeats the loop or exit
+        allParams.clear();
+        free(args);
 
     } while (codeFlag);
 }
@@ -77,15 +97,108 @@ void cwushell::mechanism() {
  *
  * @return
  */
-int cwushell::execute(vector<string> vec)  {
+int cwushell::execute( char* argv[])  {
 
     // using fork() to create a parallel process
     pid_t  pid = fork();
 
+    //char* arguments[] = const_cast<char*>(vec.);
+    if (pid == 0) {         // child process
+        execvp(reinterpret_cast<const char *>(argv[0]), reinterpret_cast<char *const *>(argv));
+        printf("***************\n");
+    } else if (pid < 0) {
+        cout << "Error in forking attempt!" << endl;
+    }
+    else {
+        int sts;
+        waitpid(pid, &sts, 0);
+    }
 
+    return 1;
+}
 
+/** -----------------------------------------------
+        Helper functions for predefined commands
+    ----------------------------------------------- */
 
+int cwushell::quit(string a, int x) {
+   // int sizeArr = sizeof(myParameters)/sizeof(myParameters[0]);
+    if (a == exit1 && x == 1) {
+        exit(0);
+    } else if ((a.find(exit1) != string::npos) && x == 2) {
+        char* code_e = myParameters[1];
+        int code_exit = atoi(code_e);
+        exit(code_exit);
+    } else{
+        printf("Wrong command. Visit help\n");
+    }
+}
 
+int cwushell::change(string b, int y) {
+    if (b == change_prompt && y == 1) {
+        default_prompt = "cwushell > ";
+        return 1;
+    } else if ((b.find(change_prompt) != string::npos) && y > 1) {
+        default_prompt = myParameters[1];
+        for (int i = 2; i < y; i++) {
+            default_prompt.append(" ");
+            default_prompt.append(myParameters[i]);
+        }
+        default_prompt.append(" > ");
+        return 1;
+    } else{
+        cout << "Invalid prompt" << endl;
+        return 0;
+    }
+}
 
-    return 0;
+int cwushell::distro(std::string c, char* ext) {
+    c.append(" ");
+    c.append(ext);
+    string c1 = "lsb_release";
+    if (c == distro_version) {
+        string c2 = "-r";
+        char* vers[3]= {const_cast<char *>(c1.c_str()), const_cast<char *>(c2.c_str()), NULL} ;
+        execute(vers);
+    } else if (c == distro_name) {
+        string c3 = "-i";
+        char* nameD[3] = {const_cast<char *>(c1.c_str()), const_cast<char *>(c3.c_str()), NULL} ;
+        execute(nameD);
+    } else if (c == distro_code) {
+        string c4 = "-c";
+        char* codeN[3] = {const_cast<char *>(c1.c_str()), const_cast<char *>(c4.c_str()), NULL} ;
+        execute(codeN);
+    }
+    else {
+        cout << "Invalid" ;
+        exit(3);
+    }
+}
+
+int cwushell::info(std::string d, char* str) {
+    d.append(" ");
+    d.append(str);
+    string a1 = "getconf";
+    if (d == info_size) {
+        // getconfig PAGESIZE
+        string a2 = "PAGE_SIZE";
+        char* arr[3] = {const_cast<char *>(a1.c_str()), const_cast<char *>(a2.c_str()), NULL} ;
+        execute(arr);
+    } else if (d == info_numbers) {
+        // getconf _AVPHYS_PAGES
+        string a3 = "_AVPHYS_PAGES";
+        char* pagesCount[3] = {const_cast<char *>(a1.c_str()), const_cast<char *>(a3.c_str()), NULL} ;
+        execute(pagesCount);
+    } else if (d == info_processors) {
+        // nproc
+        string aT = "nproc";
+        char* arr2[2] = {const_cast<char *>(aT.c_str()), NULL} ;
+        execute(arr2);
+    }
+}
+
+char * cwushell::goodFormat(std::string str) {
+    char * line_in_chars = const_cast<char*>(str.c_str());
+    char* args = strtok(line_in_chars," ");
+    return args;
 }
--- cwushell.h
+++ cwushell.h
@@ -8,11 +8,37 @@ class cwushell {
 private:
     std::string prog_name;
     std::vector<std::string> params;
+    std::string default_prompt = "cwushell > ";
+
+    // predefined commands
+    //----------- 1. quit [n]
+    std::string exit1 = "quit";
+    std::string exitAlternative;
+    //----------- 2. change prompt
+    std::string change_prompt = "change_prompt";
+    std::string redirect;
+    //----------- 3. distro -switch
+    std::string distro_version = "distro -v";
+    std::string distro_name = "distro -n";
+    std::string distro_code = "distro -c";
+    //----------- 4. info -switch
+    std::string info_size = "info -s";
+    std::string info_numbers = "info -n";
+    std::string info_processors = "info -p";
+    //----------- 5. other existing shell commands will be handled by execvp
+
+    char* myParameters[];
+
 
 public:
     cwushell();
     void mechanism();
-    int execute(std::vector<std::string> aVector);
+    int execute(char **argv);//std::string , std::vector<std::string> aVector);
+    int quit(std::string a, int code_content);
+    int change(std::string b, int y);
+    int distro(std::string c, char *string);
+    int info(std::string d, char *string);
+    char* goodFormat(std::string str);
 };
 
 
